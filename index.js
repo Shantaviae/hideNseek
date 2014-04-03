1339deb28b7abaaa1843ba5d2297a74484d000ec
francis.yang1991 <francis.yang1991@gmail.com>
2014-04-03 17:48:44 -0500

    add user location tracking

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
+	
+var prevpos;
 	function initialize() {
-		var mapOptions = {
+		var mapOptions;
+		var pos;
+		
+		 mapOptions = {
 			center: new google.maps.LatLng(-34.397, 150.644),
 			zoom: 17,
 			mapTypeControl: false,
 			streetViewControl: false
+
 		};
+	
 
 		var map = new google.maps.Map(document.getElementById("map-canvas"),
 			mapOptions);
 		var mapDiv = document.getElementById("map-canvas");
 	//	var person = prompt("Please enter your name");
 	//	var contents = person+" is here.\n Happy BirthDay! Mochen Anhe"
-	var contents = "Click here"
-	if (navigator.geolocation){
+		var contents = "Click here"
+	refreshLocation(map);
+	window.setInterval(function(){refreshLocation(map);},5000);
+	
+	}
+
+	function refreshLocation(map){
+		
+		if (navigator.geolocation){
+			console.log("geolocation");
 		navigator.geolocation.getCurrentPosition(function(position){
 			var pos  = new google.maps.LatLng(position.coords.latitude,
-				position.coords.longitude);
-			var infowindow = new google.maps.InfoWindow({
-				map: map,
-				position: pos,
-				content: contents
+				position.coords.longitude)
 
+				map.setCenter(pos);
+
+				var image = "icon-current-location.png"
+				var marker = new google.maps.Marker({
+					map:map,
+					position:pos,
+					icon: image
+
+				});
+							
+			var infowindow = new google.maps.InfoWindow({
+				content :"Got You!"
 			});
 
-			map.setCenter(pos);
-
-			google.maps.event.addDomListener(mapDiv,"click",showAlert);
-
+			google.maps.event.addDomListener(marker,"click",function(){
+				infowindow.open(map,marker);
+			});
+			
 		//========================================================	
 	},function(){
 		handleNoGeolocation(true);
@@ -37,11 +60,6 @@
 		handleNoGeolocation(false);
 	}
 	}
-
-	function showAlert(){
-		window.alert("Got You!");
-	}
-
 
 	function handleNoGeolocation(errorFlag) {
 		if (errorFlag) {
@@ -60,4 +78,7 @@
 		map.setCenter(options.position);
 	}
 
-	google.maps.event.addDomListener(window, 'load', initialize);
+	
+		google.maps.event.addDomListener(window, 'load', initialize);
+	
+	
