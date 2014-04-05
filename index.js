58a2b1bb6900832de46b637df6a5d471121a5c0f
Shantaviae <shantaviae@gmail.com>
2014-04-05 18:58:10 -0500

    Additions to front end
    Add home page (start game & join game)
    Add start game form
    Update alert css & add alertify jquery addon

--- index.js
+++ index.js
@@ -1,84 +1,154 @@
-	
-var prevpos;
-	function initialize() {
-		var mapOptions;
-		var pos;
-		
-		 mapOptions = {
-			center: new google.maps.LatLng(-34.397, 150.644),
-			zoom: 17,
-			mapTypeControl: false,
-			streetViewControl: false
+var handler;
+var divSpeed = 1000;
+var tag, timeNum;
+var gName;
+var sTime;
+var eTime;
+var numPlayers;
+var gRadius;
 
-		};
-	
+$( document ).ready(function() {
 
-		var map = new google.maps.Map(document.getElementById("map-canvas"),
-			mapOptions);
-		var mapDiv = document.getElementById("map-canvas");
-	//	var person = prompt("Please enter your name");
-	//	var contents = person+" is here.\n Happy BirthDay! Mochen Anhe"
-		var contents = "Click here"
-	refreshLocation(map);
-	window.setInterval(function(){refreshLocation(map);},5000);
-	
-	}
+	var locate = window.location;
+	document.startGameForm.gName.value = locate;
+	var gName = document.startGameForm.gName.value;
+	gName = delineate(gName);
 
-	function refreshLocation(map){
-		
-		if (navigator.geolocation){
-			console.log("geolocation");
-		navigator.geolocation.getCurrentPosition(function(position){
-			var pos  = new google.maps.LatLng(position.coords.latitude,
-				position.coords.longitude)
-
-				map.setCenter(pos);
-
-				var image = "icon-current-location.png"
-				var marker = new google.maps.Marker({
-					map:map,
-					position:pos,
-					icon: image
-
-				});
-							
-			var infowindow = new google.maps.InfoWindow({
-				content :"Got You!"
-			});
-
-			google.maps.event.addDomListener(marker,"click",function(){
-				infowindow.open(map,marker);
-			});
-			
-		//========================================================	
-	},function(){
-		handleNoGeolocation(true);
-	});
-	}
+	if (gName == null || gName == "") {
+		$("#divTimer").hide();
+		$("#map-canvas").hide();
+		$("#header").hide();
+		$("#startFormDiv").hide();
+		$("#home").show();
+	} 
 	else {
-		// Browser doesn't support Geolocation
-		handleNoGeolocation(false);
+		$("#divTimer").show();
+		$("#map-canvas").show();
+		$("#header").show();
+		$("#startFormDiv").hide();
+		$("#home").hide();
+		startGame();
 	}
+});
+
+function delineate(str)
+{
+	var theleft = str.indexOf("=") + 1;
+	var theright = str.indexOf("&");
+	return(str.substring(theleft, theright));
+}
+
+function startForm() {
+	clearForm(document.getElementById('startGameForm'));
+	$("#header").show();
+	$("#home").hide();
+	$("#startFormDiv").show();
+}
+
+function cancelForm(oForm) {
+	$("#header").hide();
+	$("#home").show();
+	$("#startFormDiv").hide();
+	clearForm(oForm);
+	
+}
+
+function startGame() {
+	
+	handler = setInterval("decrementValue()", divSpeed);
+	//send all data to the server
+}
+
+function stop(tag) {
+    clearInterval(handler);
+    if (tag == false){
+		alertify.alert('Time up! You lose!');
+        //alert("Time up! You lose!");
+    }
+    else{
+		alertify.alert('Congrats! You win!');
+        //alert("Congrats! You win!");
+    }
+}
+
+function parseTimer() {
+    
+    var time = document.getElementById('divTimer').innerHTML;
+    var times = time.split(":");
+    var cTime = parseInt(times[0], 10)*60 + parseInt(times[1], 10);
+    return cTime;
+}
+
+function revertTime(timeNum) {
+    var seconds = timeNum % 60;
+    var minutes = (timeNum - seconds) / 60;
+	
+	var pTime;
+	
+	if (seconds < 10) {
+		pTime = minutes + ":0" + seconds;
 	}
-
-	function handleNoGeolocation(errorFlag) {
-		if (errorFlag) {
-			var content = 'Error: The Geolocation service failed.';
-		} else {
-			var content = 'Error: Your browser doesn\'t support geolocation.';
-		}
-
-		var options = {
-			map: map,
-			position: new google.maps.LatLng(60, 105),
-			content: content
-		};
-
-		var infowindow = new google.maps.InfoWindow(options);
-		map.setCenter(options.position);
+	if (minutes < 10) {
+		pTime = "0" + minutes + ":" + seconds;
 	}
+	if (minutes < 10 & seconds < 10) {
+		pTime = "0" + minutes + ":0" + seconds;
+	}
+	if (minutes >= 10 & seconds >= 10) {
+		pTime = minutes + ":" + seconds;
+	}
+	
+    return pTime;
+}
 
-	
-		google.maps.event.addDomListener(window, 'load', initialize);
-	
-	
+
+function decrementValue() {
+    var curTime = parseTimer();
+    if ( curTime > 0){
+        
+        curTime = curTime - 1;
+        document.getElementById('divTimer').innerHTML = revertTime(curTime);
+    }
+    else 
+    {
+        stop(false);
+    }
+}
+
+function clearForm(oForm) {
+   
+  var elements = oForm.elements;
+   
+  oForm.reset();
+
+  for(i=0; i<elements.length; i++) {
+     
+  field_type = elements[i].type.toLowerCase();
+ 
+  switch(field_type) {
+ 
+    case "text":
+    case "password":
+    case "textarea":
+          case "hidden":  
+     
+      elements[i].value = "";
+      break;
+       
+    case "radio":
+    case "checkbox":
+        if (elements[i].checked) {
+          elements[i].checked = false;
+      }
+      break;
+
+    case "select-one":
+    case "select-multi":
+                elements[i].selectedIndex = -1;
+      break;
+
+    default:
+      break;
+  }
+    }
+}
