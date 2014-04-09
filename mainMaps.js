986779d5b87d1babaad38732948093a6383040d7
Caleb Young <menlocaleb@gmail.com>
2014-04-09 18:19:04 -0500

    Made google maps load on a function call, rather than dom listener. Now call a resize function on the map when change to map view.
    Has testing workflow of start/join game -> waiting page -> map

--- mainMaps.js
+++ mainMaps.js
@@ -18,7 +18,7 @@ var divSpeed =1000;
 
 
 
-	google.maps.event.addDomListener(window, 'load', initialize);
+	//google.maps.event.addDomListener(window, 'load', initialize);
 	function Hider(currentLocation,isTagged,marker){
 		//this.playerName = playerName;
 		//this.startTime = startTime;
@@ -68,7 +68,14 @@ var divSpeed =1000;
 			console.log("this is K "+hider.currentLocation.A+"   this is A:"+hider.currentLocation.k+ "\n");
 		});
 
+		// center map on user on resize
+		google.maps.event.addDomListener(map, 'resize', function(event) {
+			initializeLocation(map);
+		});
+
 		window.setInterval(function(){collectData(map);},1000);
+
+		return map;
 	}
 
 	// Use the DOM setInterval() function to change the offset of the symbol
