b84f5893f75ee4bac6b9c20e95d5f9159b637141
Caleb Young <menlocaleb@gmail.com>
2014-04-16 17:14:17 -0500

    started new timer code

---
+++ timer.js
@@ -0,0 +1,24 @@
+/* Javascript timer object for HideNSeek app */
+
+
+var Timer = function() {
+	var handle;
+
+	this.start = function() {
+
+	}
+
+	this.stop = function() {
+
+	}
+
+	this.reset = function(optTime) {
+
+	}
+
+	this.onStart = null;
+
+	this.onStop = null;
+
+	this.onTimeOut = null;
+};
